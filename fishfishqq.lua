28256f6d519f682b99ce6c36bf7e807c3ace5a97
activednow1 <activednow1@gmail.com>
2026-06-25 09:23:51 +0700

    Update fishfishqq.lua

--- fishfishqq.lua
+++ fishfishqq.lua
@@ -94,7 +94,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Auto Use"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Buy List"] = {
-            ["Luck Totem"] = 20,
+            ["Mutation Totem"] = 20,
             "Mutation Totem",
             "Shiny Totem",
         },
