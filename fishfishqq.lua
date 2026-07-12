ef1389f8868551fea84caec730fadd92b7cfc344
activednow1 <activednow1@gmail.com>
2026-07-12 10:43:03 +0700

    Update fishfishqq.lua

--- fishfishqq.lua
+++ fishfishqq.lua
@@ -28,7 +28,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Enabled"] = true,
         ["Whitelist Username"] = {"petanimaximal04", "petanimaximal05","petanimaximal06","petanimaximal07","petanimaximal08","petanimaximal09","petanimaximal04","aquamaxpro2019","petanimaximal10","petanimaximal11","petanimaximal12","petanimaximal13","petanimaximal14","petanimaximal15","petanimaximal16","petanimaximal17","petanimaximal18","petanimaximal19","petanimaximal20","aquawraith200885"},
         ["Category Fish"] = {
-            "Secret",
+            "X",
             -- {Tier = "Mythic", Variant = "Stone"}, -- Tier + Variant
         },
         ["Fish Name"] = {
