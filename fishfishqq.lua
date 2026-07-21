d49252bc4676368a093c1077cbf703fd54acd9bb
activednow1 <activednow1@gmail.com>
2026-07-21 21:56:17 +0700

    Update fishfishqq.lua

--- fishfishqq.lua
+++ fishfishqq.lua
@@ -11,7 +11,7 @@ _G.FishItConfig = _G.FishItConfig or {
             -- {Variant = "Leviathan Rage"}, -- Variant Only
             -- {Tier = "Secret", Variant = "Leviathan Rage"},  -- Tier + Variant
         },
-        ["Trade Timeout"] = 30,
+        ["Trade Timeout"] = 60,
         ["Auto Favorite"] = true,
         ["Auto Unfavorite"] = false,
         ["Fish Name"] = {
